da25261163572e0f5d26e9ee3e152c26b5591f05
Aleksey Savchenko <alexdx@yandex.ru>
2023-10-28 16:47:05 +0300

    review fixes

--- src/utils/apiErrorHandler.ts
+++ src/utils/apiErrorHandler.ts
@@ -6,6 +6,8 @@ export const apiErrorHandler = (error: Error) => {
   if (error.message.includes("Ошибка 401 : Cookie is not valid")) {
     AppStore.clear();
     Router.go("/");
+  } else if (error.message.includes("Ошибка 400 : User already in system")) {
+    Router.go("/messenger");
   } else {
     ModalService.show("info-modal", { message: error.message ?? "Произошла ошибка" });
   }
